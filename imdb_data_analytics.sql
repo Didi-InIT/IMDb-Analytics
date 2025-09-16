82e2af755a17dfbb1a7a6b2be251e2fcc95e3ac6
Didi-InIT <diogomarvao99@gmail.com>
2025-09-16 13:23:51 +0100

    Syntax correction + query 1 explanation + query 2

--- imdb_data_analytics.sql
+++ imdb_data_analytics.sql
@@ -10,37 +10,52 @@ ORDER BY titleid,
 
 /* 
 View to select all the movies for the region US, with a distinct prioritizing imdbDisplay as display type and
-a lower ordering value since the combination titleid/ordering is the Primary Key of title_akas table
+a lower ordering value since the combination titleid/ordering is the Primary Key of title_akas table.
 */
 
 
 -- 1) Top Films - Bayesian Rating
 
-with stats as( 
-select avg(r.averagerating) as global_mean,
-percentile_cont(0.8) within group (order by r.numvotes) as p80_nr_votes
-from us_display_title us
-join title_ratings r on r.tconst=us.titleid
+WITH stats AS( 
+SELECT AVG(r.averagerating) AS global_mean,
+PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY r.numvotes) AS p80_nr_votes
+FROM us_display_title us
+JOIN title_ratings r ON r.tconst=us.titleid
 )
 
-select us.titleid,
+SELECT us.titleid,
 us.title,
 r.numvotes,
-r.averagerating as original_rating,
+r.averagerating AS original_rating,
 round(((r.numvotes / (r.numvotes + s.p80_nr_votes)) * r.averagerating + (s.p80_nr_votes / (r.numvotes + s.p80_nr_votes)) * s.global_mean)::numeric, 2) as bayesian_rating
-from us_display_title us
-join title_ratings r on r.tconst=us.titleid
-cross join stats s
-order by bayesian_rating desc
+FROM us_display_title us
+JOIN title_ratings r ON r.tconst=us.titleid
+CROSS JOIN stats s
+ORDER BY bayesian_rating DESC
+
+/* 
+The Bayesian Rating takes into account the number of votes that a given movie received to balance out it´s rating, and thus, 
+for example, preventing movies that got a 10.0 rating with only two votes from being at the top of the scoreboard. 
+*/
 
 
--- 2) Top Films per Decade and Genre
+-- 2) Top Films per Decade/Genre
 
-
-
+SELECT
+  (b.startyear - (b.startyear % 10))::text || 's' as decade,
+  g AS genre,
+  COUNT(*) AS movies_produced
+FROM title_basics b
+CROSS JOIN LATERAL UNNEST(string_to_array(b.genres, ',')) AS g
+WHERE b.startyear IS NOT NULL
+GROUP BY decade, genre
+ORDER BY movies_produced DESC
 
 
 -- 3) Film Length Trends 
+
+
+
 
 -- 4) Top Films - Bayesian Rating
 
